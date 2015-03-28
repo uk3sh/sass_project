c1440d848a8d7d4d44101d1629068e002765f308
Ukesh Kumar <godsfear@gmail.com>
2015-03-28 17:14:23 +0530

    Bug in Paperclip Gem. Ignoring avatar from now onwards

--- Gemfile
+++ Gemfile
@@ -47,7 +47,7 @@ gem 'stripe', '1.16.1'
 
 gem 'figaro', '1.0.0'
 
-gem 'paperclip', '4.2.1'
+gem 'paperclip', '4.2'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  Paperclip.options[:command_path] = 'D:\ImageMagick-6.9.0-Q16'
+  Paperclip.options[:command_path] = 'D:\GnuWin32\bin'
+
 end
